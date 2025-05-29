56e8d47eb14e69c9c308d9f1167073e91ac32ff0
burrones-4 <antonio.oliveira.joao@escola.pr.gov.br>
2025-05-29 20:17:06 -0300

    Add files via upload

---
+++ assets/perguntas.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    criaCartao(
+        'Mamonas Assassinas', 
+        'Qual o nome do álbum de estreia dos Mamonas Assassinas?', 
+        'O álbum de estreia chama-se "Mamonas Assassinas" e foi lançado em 1995.'
+    );
+
+    criaCartao(
+        'Banda', 
+        'Qual era o nome verdadeiro do vocalista Dinho?', 
+        'O nome verdadeiro do Dinho era Alecsander Alves Leite.'
+    );
+
+    criaCartao(
+        'Músicas', 
+        'Qual música dos Mamonas é famosa pelo refrão "Pelados em Santos"?', 
+        'A música que popularizou esse refrão é "Pelados em Santos".'
+    );
+
+    criaCartao(
+        'História', 
+        'Em que ano a banda Mamonas Assassinas morreu em um acidente aéreo?', 
+        'A banda faleceu tragicamente em 1996, em um acidente de avião.'
+    );
+});
